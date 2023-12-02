6fe6a2d79c8d02a1009fa220fc492046eee20594
Raisa <r.irakoze@alustudent.com>
2023-12-02 22:05:27 +0200

    scramble list items

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
-const draggable_list = document.getElementById('draggable-list');
-const check = document.getElementById('check');
+const draggableList = document.getElementById("draggable-list");
+const check = document.getElementById("check");
 
 const richestPeople = [
-    'Jeff Bezos',
-    'Bill Gates',
-    'Warren Buffett',
-    'Bernard Arnault',
-    'Carlos Slim Helu',
-    'Amancio Ortega',
-    'Larry Ellison',
-    'Mark Zuckerberg',
-    'Michael Bloomberg',
-    'Larry Page'
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffett",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
 ];
 
-// Store listitems
-const listitems = [];
+const listItems = [];
 
 let dragStartIndex;
 
-createList();
-
-// Insert list items into DOM
 function createList() {
-    [...richestPeople]
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
 
-        .forEach((person, index) => {
-            const listItem = document.createElement('li');
+      listItem.innerHTML = `
 
-            listItem.setAttribute('data-index', index);
-
-            listItem.innerHTML = `
-                <span class="number">${index + 1}</span>
-                <div class="draggable" draggable="true">
-                    <p class="person-name">${person}</p>
-                    <i class="fas fa-grip-lines"></i>
-                </div>           
-            `;
-
-            listItems.push(listItem);
-
-            draggable_list.appendChild(listItem);
-        });
-
-           
+      <span class="number">${index + 1}</span>
+      <div class="draggable">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-lines"></i>
+      </div>
+    `;
+      listItems.push(listItem);
+      draggableList.appendChild(listItem);
+    });
 }
+createList();
