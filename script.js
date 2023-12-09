deb05b766675a247b8e3dd5fa9fde8b098676792
Raisa <r.irakoze@alustudent.com>
2023-12-09 22:44:13 +0200

    check order

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const draggableList = document.getElementById("draggable-list");
+const draggable_list = document.getElementById("draggable-list");
 const check = document.getElementById("check");
 
 const richestPeople = [
@@ -14,9 +14,12 @@ const richestPeople = [
   "Larry Page",
 ];
 
+// Store listitems
 const listItems = [];
 
 let dragStartIndex;
+
+createList();
 
 function createList() {
   [...richestPeople]
@@ -25,18 +28,89 @@ function createList() {
     .map((a) => a.value)
     .forEach((person, index) => {
       const listItem = document.createElement("li");
+
       listItem.setAttribute("data-index", index);
 
       listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
 
-      <span class="number">${index + 1}</span>
-      <div class="draggable">
-        <p class="person-name">${person}</p>
-        <i class="fas fa-grip-lines"></i>
-      </div>
-    `;
       listItems.push(listItem);
-      draggableList.appendChild(listItem);
+
+      draggable_list.appendChild(listItem);
     });
+
+  addEventListeners();
 }
-createList();
+
+function dragStart() {
+  // console.log('Event: ', 'dragstart');
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+}
+
+function dragEnter() {
+  // console.log('Event: ', 'dragenter');
+  this.classList.add("over");
+}
+
+function dragLeave() {
+  // console.log('Event: ', 'dragleave');
+  this.classList.remove("over");
+}
+
+function dragOver(e) {
+  // console.log('Event: ', 'dragover');
+  e.preventDefault();
+}
+
+function dragDrop() {
+  // console.log('Event: ', 'drop');
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
+}
+
+check.addEventListener("click", checkOrder);
